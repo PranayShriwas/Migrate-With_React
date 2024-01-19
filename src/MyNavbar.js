98c277d1de4f2b57fd6a65ce0f94e764118240fb
PranayShriwas <pranayshriwas420@gmail.com>
2024-01-19 18:19:02 +0530

    add

--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -6,7 +6,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 const MyNavbar = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="/w">Migrate</Navbar.Brand>
+            {/* <Navbar.Brand href="/">Migrate</Navbar.Brand> */}
+            <Navbar.Brand><Link to="/" className="nav-link">Migrate</Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
